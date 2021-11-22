149adfbead8af9e9acda034ef7ed4881309d8de4
Ly Nhan <lythanhnhan27294@gmail.com>
2021-11-22 14:03:55 +0700

    add compton config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -153,7 +153,7 @@ return require('packer').startup(function()
         'xiyaowong/nvim-transparent',
         config = function()
             require("transparent").setup({
-                enable = false, -- boolean: enable transparent
+                enable = true, -- boolean: enable transparent
                 extra_groups = { -- table/string: additional groups that should be clear
                     -- In particular, when you set it to 'all', that means all avaliable groups
 
