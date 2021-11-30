928c2cf6315816fb4fafc8050c4e1645adb2c92a
Ly Nhan <lythanhnhan27294@gmail.com>
2021-11-30 13:50:09 +0700

    update nord theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,14 +1,14 @@
 return function()
     local cmd = vim.cmd
-    vim.cmd('syntax on')
-    vim.cmd('set termguicolors')
-    vim.cmd('set background=dark')
+    cmd 'syntax on'
     -- vim.g.sonokai_style = 'andromeda'
     -- vim.g.sonokai_enable_italic = 1
     -- vim.g.sonokai_disable_italic_comment = 1
     -- cmd 'colorscheme sonokai'
 
-    vim.g.everforest_background = 'soft'
-    cmd 'colorscheme everforest'
+    -- vim.g.everforest_background = 'soft'
+    -- cmd 'set background=dark'
+    cmd 'colorscheme nord'
+    cmd 'set termguicolors'
 
 end
--- nvim/setting.vim
+++ nvim/setting.vim
@@ -38,6 +38,7 @@ set wildmenu
 set expandtab
 set completeopt=menuone,noselect
 set shortmess+=c
+set termguicolors
 
 "imap
 inoremap <silent> <c-l> <Right>
