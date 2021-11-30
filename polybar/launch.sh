2cd21d0f2a64f74d6888bb9e94cb8f97b8d4cce2
Ly Nhan <lythanhnhan27294@gmail.com>
2021-11-30 14:43:33 +0700

    fix

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar alg-example 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar --config=/home/tiny/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
 
