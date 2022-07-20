b346a2f529f59e8199a85060edf589c77ac01dde
Ly Thanh Nhan - SkyMavis <kevin@nhanly-SkyMavis-MBP-E.local>
2022-07-20 13:39:01 +0700

    fix: update shkd config

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -93,7 +93,7 @@ return function()
                 -- ["t"] = "open_tabnew",
                 -- ["o"] = "open_with_window_picker",
                 ["C"] = "close_node",
-                ["z"] = "close_all_nodes",
+                -- ["z"] = "close_all_nodes",
                 -- ["Z"] = "expand_all_nodes",
                 ["a"] = {
                     "add",
@@ -105,17 +105,17 @@ return function()
                 ["n"] = "add_directory", -- also accepts the optional config.show_path option like "add".
                 ["d"] = "delete",
                 ["r"] = "rename",
-                ["y"] = "copy_to_clipboard",
-                ["x"] = "cut_to_clipboard",
+                ["c"] = "copy_to_clipboard",
+                ["m"] = "cut_to_clipboard",
                 ["p"] = "paste_from_clipboard",
-                ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+                -- ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
                 -- ["c"] = {
                 --  "copy",
                 --  config = {
                 --    show_path = "none" -- "none", "relative", "absolute"
                 --  }
                 -- }
-                ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+                -- ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
                 ["q"] = "close_window",
                 ["R"] = "refresh",
                 ["?"] = "show_help"
