b357b1f82638839cb59e5191e8424b96f0ea6b01
lynhan318 <lythanhnhan27294@gmail.com>
2022-10-01 16:02:23 +0700

    feat: implement create leaderboard, milestone page

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -271,6 +271,39 @@ return require('packer').startup(function()
             })
         end
     }
+    use {'rcarriga/nvim-notify'}
+    use({
+        "vuki656/package-info.nvim",
+        requires = "MunifTanjim/nui.nvim",
+        config = function()
+            require('package-info').setup()
+
+            -- Show dependency versions
+            vim.keymap.set({"n"}, "<LEADER>ns", require("package-info").show,
+                           {silent = true, noremap = true})
+
+            -- Hide dependency versions
+            vim.keymap.set({"n"}, "<LEADER>nc", require("package-info").hide,
+                           {silent = true, noremap = true})
+
+            -- Update dependency on the line
+            vim.keymap.set({"n"}, "<LEADER>nu", require("package-info").update,
+                           {silent = true, noremap = true})
+
+            -- Delete dependency on the line
+            -- vim.keymap.set({"n"}, "<LEADER>nd", require("package-info").delete,
+            --                {silent = true, noremap = true})
+
+            -- Install a new dependency
+            vim.keymap.set({"n"}, "<LEADER>ni", require("package-info").install,
+                           {silent = true, noremap = true})
+
+            -- Install a different dependency version
+            -- vim.keymap.set({"n"}, "<LEADER>np",
+            --                require("package-info").change_version,
+            --                {silent = true, noremap = true})
+        end
+    })
     -- use {'camspiers/animate.vim'
     -- use {'camspiers/lens.vim'}
     -- use {'ggandor/lightspeed.nvim'}
