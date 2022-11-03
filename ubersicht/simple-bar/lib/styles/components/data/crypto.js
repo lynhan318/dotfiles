d1fc661c02a88c4642bf147f3fc573819fdec42f
Lý Thành Nhân <32099104+lynhan318@users.noreply.github.com>
2022-11-03 12:07:44 +0700

    feat: remove git cache

---
+++ ubersicht/simple-bar/lib/styles/components/data/crypto.js
@@ -0,0 +1,8 @@
+export const cryptoStyles = /* css */ `
+.crypto {
+  color: var(--foreground);
+  background-color: var(--minor);
+}
+.simple-bar--widgets-background-color-as-foreground .crypto {
+  background-color: transparent;
+}`;
---
+++ ubersicht/simple-bar/lib/styles/themes.js
@@ -0,0 +1,52 @@
+import * as NightShiftDark from "./themes/night-shift-dark";
+import * as NightShiftLight from "./themes/night-shift-light";
+import * as MacOSDark from "./themes/mac-os-dark";
+import * as MacOSLight from "./themes/mac-os-light";
+import * as OneDark from "./themes/one-dark";
+import * as OneLight from "./themes/one-light";
+import * as GruvboxDark from "./themes/gruvbox-dark";
+import * as GruvboxLight from "./themes/gruvbox-light";
+import * as GruvboxMaterial from "./themes/gruvbox-material";
+import * as Dracula from "./themes/dracula";
+import * as Nord from "./themes/nord";
+import * as Amarena from "./themes/amarena";
+import * as SolarizedLight from "./themes/solarized-light";
+import * as SolarizedDark from "./themes/solarized-dark";
+import * as Cisco from "./themes/cisco";
+import * as Sylens from "./themes/sylens";
+import * as SpaceDuck from "./themes/spaceduck";
+import * as MidsummerNightDark from "./themes/midsummer-night-dark";
+import * as Catppuccin from "./themes/catppuccin";
+import * as TokyoNight from "./themes/tokyo-night";
+import * as MaterialOcean from "./themes/material-ocean";
+import * as NightOwl from "./themes/night-owl";
+import * as EverforestDark from "./themes/everforest-dark";
+import * as EverforestLight from "./themes/everforest-light";
+
+
+export const collection = {
+  EverforestLight:EverforestLight.theme,
+  EverforestDark:EverforestDark.theme,
+  NightShiftDark: NightShiftDark.theme,
+  NightShiftLight: NightShiftLight.theme,
+  MacOSDark: MacOSDark.theme,
+  MacOSLight: MacOSLight.theme,
+  OneDark: OneDark.theme,
+  OneLight: OneLight.theme,
+  GruvboxDark: GruvboxDark.theme,
+  GruvboxLight: GruvboxLight.theme,
+  GruvboxMaterial: GruvboxMaterial.theme,
+  Dracula: Dracula.theme,
+  Nord: Nord.theme,
+  Amarena: Amarena.theme,
+  SolarizedLight: SolarizedLight.theme,
+  SolarizedDark: SolarizedDark.theme,
+  Cisco: Cisco.theme,
+  Sylens: Sylens.theme,
+  SpaceDuck: SpaceDuck.theme,
+  MidsummerNightDark: MidsummerNightDark.theme,
+  Catppuccin: Catppuccin.theme,
+  TokyoNight: TokyoNight.theme,
+  MaterialOcean: MaterialOcean.theme,
+  NightOwl: NightOwl.theme,
+};
