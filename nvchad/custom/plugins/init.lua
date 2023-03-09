50d0fc17073bda0c66e481cbe008ac0f67fd909b
Lý Thành Nhân <lythanhnhan27294@gmail.com>
2023-03-09 13:30:57 +0700

    feat: install winbar

--- nvchad/custom/plugins/init.lua
+++ nvchad/custom/plugins/init.lua
@@ -58,4 +58,15 @@ return {
 			vim.cmd([[hi LspInlayHint guifg=#5c6a72 guibg=NONE ]])
 		end,
 	},
+	["utilyre/barbecue.nvim"] = {
+		tag = "*",
+		requires = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		after = "nvim-web-devicons", -- keep this if you're using NvChad
+		config = function()
+			require("barbecue").setup()
+		end,
+	},
 }
--- nvchad/custom/plugins/null-ls.lua
+++ nvchad/custom/plugins/null-ls.lua
@@ -9,7 +9,7 @@ local b = null_ls.builtins
 local sources = {
 
 	-- webdev stuff
-	b.formatting.rome,
+	-- b.formatting.rome,
 
 	-- Lua
 	b.formatting.stylua,
