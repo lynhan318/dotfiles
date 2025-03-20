fcd7cf48b0964e0e1dedb1cfcd376a1b95b6c3e1
Kevin <lythanhnhan27294@gmail.com>
2025-03-20 15:11:45 +0700

    save

--- sketchybar/helpers/install.sh
+++
@@ -1,2 +0,0 @@
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/latest/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/
