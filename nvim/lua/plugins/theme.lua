5f519e53dec7ed68168e33fb8080e4168ffa243b
Kevin <lythanhnhan27294@gmail.com>
2025-08-23 13:25:41 +0700

    update active color

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,8 +1,11 @@
 return {
-  "sainnhe/everforest",
-  name = "everforest",
+  "neanias/everforest-nvim",
+  -- name = "everforest",
   priority = 1000,
   config = function()
+    require("everforest").setup({
+        transparent_background_level = 2,
+    })
     vim.cmd.colorscheme "everforest"
   end,
 }
