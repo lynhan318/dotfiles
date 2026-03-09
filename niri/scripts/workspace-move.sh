47950628f8b35c84db2437a8a705426cc27ca26b
LyNhan <lythanhnhan27294@gmail.com>
2026-03-09 09:56:18 +0700

    fix niri set window floating before move to workspace 4

--- niri/scripts/workspace-move.sh
+++ niri/scripts/workspace-move.sh
@@ -2,8 +2,13 @@
 WORKSPACE=$1
 CURRENT_OUTPUT=$(/usr/bin/niri msg -j focused-output | /usr/bin/jq -r '.name')
 
-if [ "$WORKSPACE" = "5" ]; then
+if [ "$WORKSPACE" = "4" ]; then
+    IS_FLOATING=$(/usr/bin/niri msg -j focused-window | /usr/bin/jq -r '.is_floating')
+    if [ "$IS_FLOATING" != "true" ]; then
+        /usr/bin/niri msg action move-window-to-floating
+    fi
     /usr/bin/niri msg action move-column-to-monitor "HDMI-A-1"
+    /usr/bin/niri msg action center-window
 elif [ "$CURRENT_OUTPUT" = "HDMI-A-1" ]; then
     /usr/bin/niri msg action move-column-to-monitor "DP-1"
     /usr/bin/niri msg action move-column-to-workspace "$WORKSPACE"
